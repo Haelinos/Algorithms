e5ea014e875cdd048a82e38f6ca79bfbc0c727f7
Haelinos <hznieland@outlook.com>
2025-02-27 16:17:18 +0100

    made it less hardcoded

--- Assets/Scripts/DungeonGenerator.cs
+++ Assets/Scripts/DungeonGenerator.cs
@@ -1,48 +1,81 @@
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class DungeonGenerator : MonoBehaviour 
 {
-    // Rooms
-    RectInt room = new RectInt(0,0,100,50);
 
     public bool splitHorizontally;
     int booleanConverter;
+    int width = 100;
+    int height = 50;
 
+    int newWidth;
+    int newHeight;
+
+    // Rooms
+    RectInt room;
+
+    private void Start()
+    {
+        room = new RectInt(0, 0, width, height);
+    }
 
     void Update() 
     {
         AlgorithmsUtils.DebugRectInt(room, Color.magenta);
-        SplitDirection();
-        //SplitRandomizer();
+        SplitRandomizer();
     }
 
-    //void SplitRandomizer() 
-    //{
-    //    booleanConverter = Random.Range(0, 2);
-    //    Debug.Log(booleanConverter);
+    void SplitRandomizer()
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            booleanConverter = Random.Range(0, 2);
+            Debug.Log(booleanConverter);
+        }
 
-    //    if (booleanConverter == 1)
-    //    {
-    //        splitHorizontally = true;
-    //    }
-    //    else 
-    //    {
-    //        splitHorizontally = false;
-    //    }
-    //}
+        if (booleanConverter == 1)
+        {
+            splitHorizontally = true;
+        }
+        else
+        {
+            splitHorizontally = false;
+        }
+        SplitDirection();
+    }
+
     void SplitDirection() 
     {
         if (splitHorizontally)
         {
-            RectInt room = new RectInt(0,0,100,25);
+            RectInt room = new RectInt(0,0,width,height/2);
             AlgorithmsUtils.DebugRectInt(room, Color.magenta);
         }
         else if(!splitHorizontally) 
         {
-            RectInt room = new RectInt(0, 0, 50, 50);
+            RectInt room = new RectInt(0, 0, width/2, height);
             AlgorithmsUtils.DebugRectInt(room, Color.magenta);
+
         }
 
+        SplitSmallerRoom();
+    }
+
+    void SplitSmallerRoom()
+    {
+        if (splitHorizontally)
+        {
+            newHeight = room.height / 4;
+            RectInt room2 = new RectInt(0, 0, width, newHeight);
+            AlgorithmsUtils.DebugRectInt(room2, Color.magenta);
+        }
+        if (!splitHorizontally)
+        {
+            newWidth = room.width / 4;
+            RectInt room2 = new RectInt(0, 0, newWidth, height);
+            AlgorithmsUtils.DebugRectInt(room2, Color.magenta);
+        }
     }
 }
