00878312cd48489a0c27f50b765ade3e9187b932
Haelinos <hznieland@outlook.com>
2025-02-27 15:57:05 +0100

    Added vertical and horizontal split

--- Assets/Scripts/DungeonGenerator.cs
+++ Assets/Scripts/DungeonGenerator.cs
@@ -1,18 +1,48 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class DungeonGenerator : MonoBehaviour 
 {
+    // Rooms
     RectInt room = new RectInt(0,0,100,50);
+
+    public bool splitHorizontally;
+    int booleanConverter;
 
 
     void Update() 
     {
         AlgorithmsUtils.DebugRectInt(room, Color.magenta);
-        GenerateRect();
+        SplitDirection();
+        //SplitRandomizer();
     }
 
-    void GenerateRect() 
-    { 
-        
+    //void SplitRandomizer() 
+    //{
+    //    booleanConverter = Random.Range(0, 2);
+    //    Debug.Log(booleanConverter);
+
+    //    if (booleanConverter == 1)
+    //    {
+    //        splitHorizontally = true;
+    //    }
+    //    else 
+    //    {
+    //        splitHorizontally = false;
+    //    }
+    //}
+    void SplitDirection() 
+    {
+        if (splitHorizontally)
+        {
+            RectInt room = new RectInt(0,0,100,25);
+            AlgorithmsUtils.DebugRectInt(room, Color.magenta);
+        }
+        else if(!splitHorizontally) 
+        {
+            RectInt room = new RectInt(0, 0, 50, 50);
+            AlgorithmsUtils.DebugRectInt(room, Color.magenta);
+        }
+
     }
 }
