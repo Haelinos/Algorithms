8a9d12e97e5d9895857f17457353cdf253ac87e3
Haelinos <hznieland@outlook.com>
2025-04-13 23:50:39 +0200

    Added loop that checks the rooms
    Added a for loop to check if the rooms can still be split until minimum room size.

--- Assets/Scripts/DungeonGenerator.cs
+++ Assets/Scripts/DungeonGenerator.cs
@@ -11,36 +11,16 @@ public class DungeonGenerator : MonoBehaviour
 
     private int minRoomSize = 6;
 
-
     RectInt startRoom;
     private List<RectInt> rooms = new List<RectInt>();
-
-    private int newWidth;
-    private int newWidthOffset;
-    private int newHeight;
-    private int newHeightOffset;
-    private int newX;
-    private int newY;
-    public int offset;
 
     private void Start()
     {
         GenerateDungeon();
     }
 
-    /*
-    void Update() 
-    {
-        // Colors the rooms
-        foreach (var room in rooms)
-        {
-            AlgorithmsUtils.DebugRectInt(room, Color.magenta);
-        }
 
-        AlgorithmsUtils.DebugRectInt(startRoom, Color.green);
-    }
-    */
-
+    // Split and gerenate the dungeon
     [Button]
     void GenerateDungeon() 
     {
@@ -52,6 +32,7 @@ public class DungeonGenerator : MonoBehaviour
         var splitResult = SplitVertically(startRoom);
         //var splitResult = SplitHorizontally(startRoom);
 
+
         // Splits rooms vertically
         var splitResultVertical = SplitVertically(splitResult.Item1);
 
@@ -59,29 +40,59 @@ public class DungeonGenerator : MonoBehaviour
         DebugDrawingBatcher.BatchCall(() => AlgorithmsUtils.DebugRectInt(splitResultVertical.Item2, Color.yellow));
 
         // Splits rooms horizontally
-        var splitResultHorizontal = SplitHorizontally(splitResult.Item2);
+        var splitResultHorizontal = SplitHorizontally(splitResult.Item1);
 
         DebugDrawingBatcher.BatchCall(() => AlgorithmsUtils.DebugRectInt(splitResultHorizontal.Item1, Color.blue));
         DebugDrawingBatcher.BatchCall(() => AlgorithmsUtils.DebugRectInt(splitResultHorizontal.Item2, Color.blue));
+
+        rooms.Add(startRoom);
+        rooms.Add(splitResultVertical.Item1);
+        rooms.Add(splitResultVertical.Item2);
+        rooms.Add(splitResultHorizontal.Item1);
+        rooms.Add(splitResultHorizontal.Item2);
+
+        // Goes through list of rooms to check if the rooms aren't too small to split
+        for (int i = 0; i < rooms.Count; i++)
+        {
+            RectInt room = rooms[i];
+
+            if ((room.width > (minRoomSize * 2) + 1 && room.height > (minRoomSize * 2) + 1))
+            {
+                Debug.Log("Can still be split");
+            }
+        }
+
     }
 
+    // Splits the given rooms into two new rooms (vertical)
     (RectInt,RectInt) SplitVertically(RectInt pRoom)
     {
         int newWidth = Random.Range(minRoomSize, pRoom.width - minRoomSize);
 
-        RectInt roomVerticalSplit = new RectInt(pRoom.x, pRoom.y, newWidth+1, pRoom.height);
-        RectInt roomVerticalSplit2 = new RectInt(pRoom.x + newWidth, pRoom.y, pRoom.width - newWidth, pRoom.height);
+        RectInt roomVSplit = new RectInt(pRoom.x, pRoom.y, newWidth + 1, pRoom.height);
+        RectInt roomVSpit2 = new RectInt(pRoom.x + newWidth, pRoom.y, pRoom.width - newWidth, pRoom.height);
 
-        return (roomVerticalSplit, roomVerticalSplit2);
+        return (roomVSplit, roomVSpit2);
     }
 
+    // Splits the given rooms into two new rooms (horizontal)
     (RectInt, RectInt) SplitHorizontally(RectInt pRoom)
     {
         int newHeight = Random.Range(minRoomSize, pRoom.height - minRoomSize);
 
-        RectInt roomHorizontalSplit = new RectInt(pRoom.x, pRoom.y, pRoom.width, newHeight + 1);
-        RectInt roomHorizontalSplit2 = new RectInt(pRoom.x, pRoom.y + newHeight, pRoom.width, pRoom.height - newHeight);
+        RectInt roomHSplit = new RectInt(pRoom.x, pRoom.y, pRoom.width, newHeight + 1);
+        RectInt roomHSplit2 = new RectInt(pRoom.x, pRoom.y + newHeight, pRoom.width, pRoom.height - newHeight);
 
-        return (roomHorizontalSplit, roomHorizontalSplit2);
+        return (roomHSplit, roomHSplit2);
     }
-}
+
+    [Button]
+    void ListContentDebug()
+    {
+        Debug.Log("Current rooms in the list:");
+        foreach (var room in rooms)
+        {
+            Debug.Log(room);
+        }
+    }
+} 
